6b198498796cc64540a1e9ac0225c25692f7492a
DariusGuillaume <dariusguillaume2@gmail.com>
2024-05-19 00:33:23 -0400

    added topline header

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import "../../App.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header style={{ backgroundColor: 'var(--navy-500)' }} className="py-3">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <p style={{ color: 'white' }}>
+              Free Shipping Over $100 & Free Returns
+            </p>
+          </div>
+          <div>
+            <p style={{ color: 'white', textAlign: 'right' }}>
+              Hotline: <a href="tel:1-800-123-4567" style={{ color: 'white' }}>1-800-123-4567</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
